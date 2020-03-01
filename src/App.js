ca788e1d509250eb1b4a5d558e4dac6da4abcd63
Kateryna Strough <katerynastrough@hotmail.com>
2020-03-01 23:46:33 +0000

    Automatically backed up by Learn

--- src/App.js
+++ src/App.js
@@ -21,6 +21,37 @@ class App extends Component {
   }
 };
 
+import React, { Component } from 'react';
+import './App.css';
+import { connect } from 'react-redux';
+import { addItem } from  './actions/items';
+
+class App extends Component {
+
+  handleOnClick() {
+    this.props.store.dispatch(addItem());
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <button onClick={(event) => this.handleOnClick(event)}>
+          Click
+          </button>
+        <p>{this.props.items.length}</p>
+      </div>
+    );
+  }
+};
+
+const mapStateToProps = (state) => {
+  return {
+    items: state.items
+  };
+};
+
+export default connect(mapStateToProps)(App);
+
 const mapStateToProps = (state) => {
   return {
     items: state.items
