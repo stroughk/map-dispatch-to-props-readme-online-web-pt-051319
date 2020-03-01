ab2f2298638737ea74c3d3514ca90f2c474756ec
Kateryna Strough <katerynastrough@hotmail.com>
2020-03-01 23:46:54 +0000

    Done.

--- src/App.js
+++ src/App.js
@@ -21,10 +21,34 @@ class App extends Component {
   }
 };
 
+import React, { Component } from 'react';
+import './App.css';
+import { connect } from 'react-redux';
+import { addItem } from  './actions/items';
+
+class App extends Component {
+
+  handleOnClick() {
+    this.props.store.dispatch(addItem());
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <button onClick={(event) => this.handleOnClick(event)}>
+          Click
+          </button>
+        <p>{this.props.items.length}</p>
+      </div>
+    );
+  }
+};
+
 const mapStateToProps = (state) => {
   return {
     items: state.items
   };
 };
 
-export default connect(mapStateToProps)(App);
+
+export default connect(mapStateToProps, mapDispatchToProps)(App);
